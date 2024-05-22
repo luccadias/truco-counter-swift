fee8852a113494a133bff275a4876f892a8d6c70
luccadias <luccadiass@gmail.com>
2024-05-22 20:40:21 -0300

    feat: add app initial config default by xcode

---
+++ truco-counter/truco_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  truco_counterApp.swift
+//  truco-counter
+//
+//  Created by Lucca Dias on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct truco_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
